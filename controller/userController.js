abc97392bb19d6bf84922198f0a127f43a0e7d72
Kaushil <Kaushil.Rambhia@ril.com>
2018-10-15 19:59:18 +0530

    test

--- controller/userController.js
+++ controller/userController.js
@@ -9,7 +9,30 @@ exports.userUpdate = function( req, res ){
 	userModel.update( req , res );
 };
 
-
 exports.userDelete = function( req, res ){
-	userModel.delete( req , res );
+
+	const response = userModel.delete( req , res );
+	
+	response.then( value =>{
+		console.log(value);
+	},error => {
+		console.log(error);
+	}
+	);
+
+	/*response.then((value)=>{
+    console.log( value );
+	if( value.status == false ){
+		res.status(400); 
+		res.type('json'); 
+		res.send({"message":value.message});
+	}else{
+		res.status(200); 
+		res.type('json'); 
+		res.send({"message": value.message });
+	}
+	},*/
+
+	
+    //return value;
 };
--- models/userModel.js
+++ models/userModel.js
@@ -6,20 +6,116 @@ exports.create = ( req , res ) =>{
 
 	const userData = [ req.body.name, req.body.age, req.body.city ];
 
-	const insertQuery = "INSERT INTO user_master ( name , age , city ) values ? ? ? ";
+	const insertQuery = "INSERT INTO user_master ( name , age , city ) values ( ? , ? , ? ) ";
 	//mysqlService.connect() ;
 	mysqlService.connect().query( insertQuery , userData , ( error , results, fields )=>{
 		if( error ) throw error;
 		console.log( results );
+
+		if( results.affectedRows == 1 ){
+			res.status(200); 
+			res.type('json'); 
+			res.send({"message":'Created successfully'});
+		}
 	})
 }
 
 exports.delete = ( req , res ) =>{
+
+	return new Promise( ( resolve , reject ) =>{
+		exports.exists( req , ( error , result ) =>{
+			
+				return resolve( result );
+			
+		});
+	}).then( ( data)=>{
+		
+		if( data.length == 1 ){
+
+			//logic to soft delete user
+			const deleteRowId = [ req.params.userId ];
+			const deleteQuery = "UPDATE user_master set STATUS = '0' where id = ? ";
+			console.log("38");
+
+			mysqlService.connect().query( deleteQuery, deleteRowId ,( error , results , fields )=>{
+				if( error ) throw error;
+				if( results.affectedRows == 1 ){
+					return {'status': true , "message" : 'Delete successfully' };
+				}
+			});	
+			console.log("45");	
+
+		}else{
+			throw("User Doesnot exists pppopppo");
+		}	
+
+	}).catch( (error )=>{
+		return {'status': false , "message" : `${error}` };
+	});	
+
 
 
 }
 
 exports.update = ( req , res ) => {
 
+	return new Promise( ( resolve , reject ) =>{
+		exports.exists( req , ( error , result ) =>{
+			if( error ) return reject( error );
+			else{
+				resolve( result );
+			}
+		});
+	}).then( ( data)=>{
 
+		if( data.length == 1 ){
+			
+			const whereClause = {
+				id : req.params.userId
+			}
+
+			const updateData = req.body;
+			
+			const updateQuery = "UPDATE user_master set ? where ? ";
+
+			mysqlService.connect().query( updateQuery , [ updateData , whereClause ] , ( error ,  results ,fields ) =>{
+				if( error ) throw error;
+				if( results.affectedRows == 1 ){
+					res.status(200); 
+					res.type('json'); 
+					res.send({"message":'Updated successfully'});
+				}
+
+			});
+
+
+		}else{
+			throw("User Does not exists");
+		}
+	}).catch( (error )=>{
+		res.status(400); 
+		res.type('json'); 
+		res.send({"message":`${error}`});
+	});
+
+}
+
+exports.exists = ( req , callback ) => {
+
+	const whereClause = {
+		id : req.params.userId
+	}
+
+	const selectQuery = "select id from user_master where ? ";
+
+	mysqlService.connect().query( selectQuery , whereClause ,( error ,  results )=>{
+
+		if( error ){
+			return callback( error );
+		}else{
+			return callback(null,results )
+		}
+
+	});
+	
 }
